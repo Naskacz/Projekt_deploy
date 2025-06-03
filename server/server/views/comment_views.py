ab562347b4b2aaf593d44ca433e354037de2ef4f
Naskacz <milosz.hart@gmail.com>
2025-06-03 11:23:31 +0200

    wyswietlanie komentarzy

--- server/server/views/comment_views.py
+++ server/server/views/comment_views.py
@@ -24,3 +24,11 @@ def delete_comment(request, comment_id):
     comment = get_object_or_404(Comment, id=comment_id, creator=request.user)
     comment.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_comments(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    comments = Comment.objects.filter(post=post).order_by('create_date')
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
